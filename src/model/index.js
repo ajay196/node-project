670fdc1fbb2daebae80a6ed15e9abb6417cf38de
ajay <ajay.singh@absolutefoods.in>
2022-05-27 16:09:47 +0530

    basic update

---
+++ src/model/index.js
@@ -0,0 +1,14 @@
+'use strict'
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize(process.env.Database, process.env.User, process.env.Password,
+    {host: process.env.Host,port: process.env.Port, dialect: 'postgres'})
+sequelize.authenticate().then(()=>{console.log('Connection has been established successfully.')})
+  const db = {}
+  db.sequelize = sequelize
+ module.exports = { db}
+
+
+
+
+
