9ae97856cb76d836e05dd3d209cac15edce2ef27
ajay <ajay.singh@absolutefoods.in>
2022-06-03 10:51:18 +0530

    qr image rendering

--- src/controller/qrcodeController.js
+++ src/controller/qrcodeController.js
@@ -2,14 +2,19 @@ const res = require('express/lib/response')
 const QRCode = require('qrcode')
 async function generateqr(req,res) {
     let data = {
-        name:"Employee Name",
-        age:27,
-        department:"Police",
-        id:"aisuoiqu3234738jdhf100223"
+        name:"Ajay kumar Singh",
+        age:24,
+        designation:"Software Developer"
     }
     let stringdata = JSON.stringify(data)
     const file =await  QRCode.toDataURL(stringdata)
-    return res.send(file)
+    const qrImage = `<!DOCTYPE html>
+    <html>
+    <body>
+    <img src="${file}" alt="ajay_qr">
+    </body>
+    </html>`
+    return res.send(qrImage)
 }
 
 module.exports = {generateqr}
