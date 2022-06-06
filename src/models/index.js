27f446bc039a5fbe9e1085f78905668a01151af8
ajay <ajay.singh@absolutefoods.in>
2022-06-06 14:15:54 +0530

    db configration

--- src/models/index.js
+++ src/models/index.js
@@ -3,7 +3,7 @@ const env = process.env.NODE_ENV || 'development'
 const config = require('../db/config.json')
 const Sequelize = require('sequelize')
 const currentEnv = config[env]
-
+console.log(currentEnv);
 const sequelize = new Sequelize(currentEnv.database, currentEnv.username, currentEnv.password, currentEnv)
 sequelize.authenticate().then(() => { console.log('Connection has been established successfully.') }).catch((err) => { console.log(err)})
 const db = {}
